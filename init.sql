4da7b726150f527bb31b81b66f89338af5567f02
Renan Martins <renan@renan>
2024-02-18 12:26:52 -0300

    Update files

--- init.sql
+++ init.sql
@@ -10,3 +10,12 @@ VALUES (1, 100000, 0),
        (3, 1000000, 0),
        (4, 10000000, 0),
        (5, 500000, 0);
+
+CREATE TABLE Transacao (
+                           id SERIAL PRIMARY KEY,
+                           conta_id INTEGER,
+                           valor INTEGER NOT NULL,
+                           tipo VARCHAR(255) NOT NULL,
+                           descricao VARCHAR(255),
+                           realizada_em TIMESTAMP
+);
